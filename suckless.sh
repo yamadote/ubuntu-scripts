183c6225d473da8fc9a4c3b7ab2139a9dbdfc323
Роман Кривенко <yamadote@gmail.com>
2022-03-15 21:58:10 +0200

    Update suckless.sh

--- suckless.sh
+++ suckless.sh
@@ -1,18 +1,6 @@
 
-# sudo apt install x11-xserver-utils
-
-# xrandr -s 1440x900
-# xrandr --output DVI-D-0 --mode 1920x1080 --output HDMI-0 --mode 1920x1080 --output DVI-D-0 --left-of HDMI-0
-
 ## DWM
 
-sudo apt install build-essential libx11-dev libxft-dev libxinerama-dev xorg libglib2.0-dev libwebkit2gtk-4.0-dev xserver-xorg-dev 
-
-sudo apt install neofetch mlocate
-sudo apt install xorg libx11-dev libxft-dev libxinerama-dev libwebkit2gtk-4.0-dev
-
-# sudo apt install surf grc libglib2.0-dev libgtk2.0-0 libgcr-3-dev webkit2gtk-4.0
-
+# xrandr --output DVI-D-0 --mode 1920x1080 --output HDMI-0 --mode 1920x1080 --output DVI-D-0 --left-of HDMI-0
+sudo apt install xorg libx11-dev libxft-dev libxinerama-dev
 sudo apt install sxiv
-
-sudo apt install compton compton-conf feh
